af5b52e20da0140ab37bb903222a71fa1d5f43a5
frenkymahn <130400270+frenkymahn@users.noreply.github.com>
2026-09-08 17:10:37 +0530

    Update jump_label.h

--- arch/arm64/include/asm/jump_label.h
+++ arch/arm64/include/asm/jump_label.h
@@ -1,53 +1,45 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/*
- * Copyright (C) 2013 Huawei Ltd.
- * Author: Jiang Liu <liuj97@gmail.com>
- *
- * Based on arch/arm/include/asm/jump_label.h
- */
-#ifndef __ASM_JUMP_LABEL_H
-#define __ASM_JUMP_LABEL_H
+#ifndef _ASM_JUMP_LABEL_H
+#define _ASM_JUMP_LABEL_H
 
 #ifndef __ASSEMBLY__
 
 #include <linux/types.h>
 #include <asm/insn.h>
 
-#define JUMP_LABEL_NOP_SIZE		AARCH64_INSN_SIZE
+#define JUMP_LABEL_NOP_SIZE AARCH64_INSN_SIZE
 
-static __always_inline bool arch_static_branch(struct static_key *key,
-					       bool branch)
+static __always_inline bool arch_static_branch(struct static_key *key, bool branch)
 {
-asm_volatile_goto(
-    "1: nop \n\t"
-    ".pushsection __jump_table, \"aw\" \n\t"
-    ".align 3 \n\t"
-    ".long 1b - ., l_yes - . \n\t"
-    ".quad %c0 \n\t"
-    ".popsection \n\t"
-    : : "r"(&((char *)key)[branch]) : : l_yes);
+    asm_volatile_goto(
+        "1: nop \n\t"
+        ".pushsection __jump_table, \"aw\" \n\t"
+        ".align 3 \n\t"
+        ".long 1b - ., %l[l_yes] - . \n\t"
+        ".quad %c0 \n\t"
+        ".popsection \n\t"
+        : : "r"(&((char *)key)[branch]) : : l_yes);
 
-	return false;
+    return false;
 l_yes:
-	return true;
+    return true;
 }
 
-static __always_inline bool arch_static_branch_jump(struct static_key *key,
-						    bool branch)
+static __always_inline bool arch_static_branch_jump(struct static_key *key, bool branch)
 {
-asm_volatile_goto(
-    "1: b l_yes \n\t"
-    ".pushsection __jump_table, \"aw\" \n\t"
-    ".align 3 \n\t"
-    ".long 1b - ., l_yes - . \n\t"
-    ".quad %c0 \n\t"
-    ".popsection \n\t"
-    : : "r"(&((char *)key)[branch]) : : l_yes);
+    asm_volatile_goto(
+        "1: b %l[l_yes] \n\t"
+        ".pushsection __jump_table, \"aw\" \n\t"
+        ".align 3 \n\t"
+        ".long 1b - ., %l[l_yes] - . \n\t"
+        ".quad %c0 \n\t"
+        ".popsection \n\t"
+        : : "r"(&((char *)key)[branch]) : : l_yes);
 
-	return false;
+    return false;
 l_yes:
-	return true;
+    return true;
 }
 
-#endif  /* __ASSEMBLY__ */
-#endif	/* __ASM_JUMP_LABEL_H */
+#endif /* __ASSEMBLY__ */
+#endif /* _ASM_JUMP_LABEL_H */
