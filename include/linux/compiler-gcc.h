912261f87cb7621ee46d074de3b7268c1e73ce93
frenkymahn <130400270+frenkymahn@users.noreply.github.com>
2026-09-08 06:35:43 +0530

    Refactor asm_volatile_goto macro for simplicity
    Removed unnecessary asm statement in asm_volatile_goto macro.

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -104,8 +104,7 @@
  *
  * (asm goto is automatically volatile - the naming reflects this.)
  */
-#define asm_volatile_goto(x...)	do { asm goto(x); asm (""); } while (0)
-
+#define asm_volatile_goto(x...) do { asm goto(x); } while (0)
 /*
  * sparse (__CHECKER__) pretends to be gcc, but can't do constant
  * folding in __builtin_bswap*() (yet), so don't set these for it.
