a51e9f4bd236612bc65c142ce51a3b36e41a7f77
frenkymahn <130400270+frenkymahn@users.noreply.github.com>
2026-09-08 16:32:50 +0530

    Refactor assembly code formatting in jump_label.h

--- arch/arm64/include/asm/jump_label.h
+++ arch/arm64/include/asm/jump_label.h
@@ -18,14 +18,14 @@
 static __always_inline bool arch_static_branch(struct static_key *key,
 					       bool branch)
 {
-	asm_volatile_goto(
-		"1:	nop					\n\t"
-		 "	.pushsection	__jump_table, \"aw\"	\n\t"
-		 "	.align		3			\n\t"
-		 "	.long 1b - ., l_yes - . \n\t
-		 "	.quad		%c0 - .			\n\t"
-		 "	.popsection				\n\t"
-		 :  :  "r"(&((char *)key)[branch]) :  : l_yes);
+asm_volatile_goto(
+    "1: nop \n\t"
+    ".pushsection __jump_table, \"aw\" \n\t"
+    ".align 3 \n\t"
+    ".long 1b - ., l_yes - . \n\t"
+    ".quad %c0 \n\t"
+    ".popsection \n\t"
+    : : "r"(&((char *)key)[branch]) : l_yes);
 
 	return false;
 l_yes:
@@ -35,14 +35,14 @@ l_yes:
 static __always_inline bool arch_static_branch_jump(struct static_key *key,
 						    bool branch)
 {
-	asm_volatile_goto(
-		"1: b l_yes \n\t
-		 "	.pushsection	__jump_table, \"aw\"	\n\t"
-		 "	.align		3			\n\t"
-		 "	 .long 1b - ., l_yes - . \n\t
-		 "	.quad		%c0 - .			\n\t"
-		 "	.popsection				\n\t"
-		 :  :  "r"(&((char *)key)[branch]) :  : l_yes);
+asm_volatile_goto(
+    "1: b l_yes \n\t"
+    ".pushsection __jump_table, \"aw\" \n\t"
+    ".align 3 \n\t"
+    ".long 1b - ., l_yes - . \n\t"
+    ".quad %c0 \n\t"
+    ".popsection \n\t"
+    : : "r"(&((char *)key)[branch]) : l_yes);
 
 	return false;
 l_yes:
