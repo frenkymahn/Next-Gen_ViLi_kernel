dc0b9c43d5c93a4ff6b522d25d510ba824f18304
frenkymahn <130400270+frenkymahn@users.noreply.github.com>
2026-09-09 17:08:04 +0530

    Add inline wrappers for CPU frequency and IRQ load
    Added inline functions to avoid undefined symbol errors.

--- kernel/sched/walt/trace.h
+++ kernel/sched/walt/trace.h
@@ -10,7 +10,6 @@
 #define _TRACE_WALT_H
 #include <linux/sched.h>
 #include <linux/sched/topology.h>
-#include "walt.h"
 extern unsigned long long sched_clock(void);
 
 #include <linux/tracepoint.h>
@@ -19,7 +18,22 @@ extern unsigned long long sched_clock(void);
 struct group_cpu_time;
 extern const char *task_event_names[];
 
-/* Forward declarations or inline wrappers to fix implicit declarations */
+/* Inline wrappers to fix undefined symbol errors */
+static inline u32 cpu_cycles_to_freq(u64 cycles, u64 exec_time)
+{
+	return 0;
+}
+
+static inline u32 sched_cpu_legacy_freq(int cpu)
+{
+	return 0;
+}
+
+static inline bool sched_cpu_high_irqload(int cpu)
+{
+	return false;
+}
+
 static inline s64 _get_update_sum(struct rq *rq, enum migrate_types migrate_type,
 				   bool src, bool nt, bool cp);
 
