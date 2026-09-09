5b1b81e181a5704bfd88a45fcbf16ca63258284d
frenkymahn <130400270+frenkymahn@users.noreply.github.com>
2026-09-09 18:05:06 +0530

    Fix jump label assembly instructions for ARM64

--- arch/arm64/include/asm/jump_label.h
+++ arch/arm64/include/asm/jump_label.h
@@ -15,10 +15,10 @@ static __always_inline bool arch_static_branch(struct static_key *key, bool bran
         "1: nop \n\t"
         ".pushsection __jump_table, \"aw\" \n\t"
         ".align 3 \n\t"
-        ".long 1b - ., . - . \n\t"
-        ".quad 0 \n\t"
+        ".long 1b - ., %l[l_yes] - . \n\t"
+        ".quad %c0 - . \n\t"
         ".popsection \n\t"
-        : : "r"(&((char *)key)[branch]) : : l_yes);
+        : : "i"(&((char *)key)[branch]) : : l_yes);
 
     return false;
 l_yes:
@@ -28,13 +28,13 @@ l_yes:
 static __always_inline bool arch_static_branch_jump(struct static_key *key, bool branch)
 {
     asm_volatile_goto(
-        "1: b l_yes \n\t"
+        "1: b %l[l_yes] \n\t"
         ".pushsection __jump_table, \"aw\" \n\t"
         ".align 3 \n\t"
-        ".long 1b - ., . - . \n\t"
-        ".quad 0 \n\t"
+        ".long 1b - ., %l[l_yes] - . \n\t"
+        ".quad %c0 - . \n\t"
         ".popsection \n\t"
-        : : "r"(&((char *)key)[branch]) : : l_yes);
+        : : "i"(&((char *)key)[branch]) : : l_yes);
 
     return false;
 l_yes:
