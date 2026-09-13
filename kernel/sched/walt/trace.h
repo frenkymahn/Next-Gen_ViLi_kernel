35c3ac3e7bd603ff573b1532026b10593165628d
frenkymahn <130400270+frenkymahn@users.noreply.github.com>
2026-09-13 11:24:32 +0530

    Add inline function for CPU high IRQ load check
    Added inline function to check CPU high IRQ load.

--- kernel/sched/walt/trace.h
+++ kernel/sched/walt/trace.h
@@ -18,6 +18,15 @@ extern unsigned long long sched_clock(void);
 struct group_cpu_time;
 extern const char *task_event_names[];
 
+/* Safe inline declaration to prevent implicit declaration error */
+#ifndef _SCHED_CPU_HIGH_IRQLOAD_DEFINED
+#define _SCHED_CPU_HIGH_IRQLOAD_DEFINED
+static inline bool sched_cpu_high_irqload(int cpu)
+{
+	return false;
+}
+#endif
+
 static inline s64 _get_update_sum(struct rq *rq, enum migrate_types migrate_type,
 				   bool src, bool nt, bool cp);
 
